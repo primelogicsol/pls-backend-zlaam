404c2a425bb85d4073f9f12f1f120497554055de
mr-zlaam <zlaam.dev@gmail.com>
2024-10-26 16:42:55 +0500

    checking husky is working

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,16 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
   {
+    ignores: ["dist/", "dist", "node_modules/", ".env", ".gitignore", "tsconfig.json", "./.env", ".env.example"],
+
     rules: {
       eqeqeq: "off",
       "no-unused-vars": "error",
       "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "prefer-arrow-callback": ["error"],
+      "camelcase": ["error", { "properties": "always" }]
     },
   },
 ];
--- server.ts
+++ server.ts
@@ -3,7 +3,8 @@ import process from "node:process";
 import type { Express } from "express";
 const app: Express = express();
 const port = process?.env?.PORT || 4000;
+let hero_name = "zlaam"
 app.get("/", (_, res) => {
   res.send("Hello World!!");
 });
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port} ${hero_name}`));
