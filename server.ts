0fa2eeaf801acbf3612f00f21e41978a588c613f
mr-zlaam <zlaam.dev@gmail.com>
2024-10-26 05:04:20 +0500

    setup linting and formating

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import process from "node:process";
 import type { Express } from "express";
 const app: Express = express();
+const port = process?.env?.PORT || 4000;
 app.get("/", (_, res) => {
-  res.send("Hello World ");
+  res.send("Hello World!!");
 });
-app.listen(3000, () => console.log("listening on port 3000"));
+app.listen(port, () => console.log(`listening on port ${port}`));
