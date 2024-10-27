f22baa7077d5208d492247fa8f8a77c1db0eb061
mr-zlaam <zlaam.dev@gmail.com>
2024-10-27 20:29:07 +0500

    chore(services & middlewares): configure global error middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import path from "node:path";
 import { authRouter } from "./routers/authRouter/authRouter";
+import { errorHandler, notFoundHandler } from "./middlewares/errorMiddleware";
 // **** APP *****
 const app: Express = express();
 // ** MIDDLEWARES **
@@ -11,4 +12,8 @@ app.use(express.static(path.join(__dirname, "../", "public")));
 app.use(cors());
 // ** ROUTES **
 app.use("/api/v1/auth", authRouter);
+
+// **** ERROR HANDLERS ****
+app.use(notFoundHandler);
+app.use(errorHandler);
 export { app };
--- src/controllers/authController/authController.ts
+++ src/controllers/authController/authController.ts
@@ -2,6 +2,8 @@ import type { Request, Response } from "express";
 import { httpResponse } from "../../utils/apiResponseUtils";
 export default {
   self: (req: Request, res: Response) => {
+    const her = false;
+    if (!her) throw { status: 400, message: "Bad request" };
     httpResponse(req, res, 200, "process was success", null);
   }
 };
