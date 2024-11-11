9efd15966e39c37cacf17772c7e6bd3e1f0e0399
zlaam <zlaam.dev@gmail.com>
2024-11-11 21:28:16 +0500

    feat(hireus): Add hire us feature

--- src/controllers/hireUsController/hireUsController.ts
+++ src/controllers/hireUsController/hireUsController.ts
@@ -1,15 +1,17 @@
 import type { UploadApiResponse } from "cloudinary";
 import { uploadOnCloudinary } from "../../services/cloudinaryService";
 import { asyncHandler } from "../../utils/asyncHandlerUtils";
-import { ADMINNAME, BADREQUESTCODE, HIREUSMESSAGE, SUCCESSCODE, SUCCESSMSG } from "../../constants";
+import { ADMINNAME, BADREQUESTCODE, HIREUSMESSAGE, NOTFOUNDCODE, NOTFOUNDMSG, SUCCESSCODE, SUCCESSMSG } from "../../constants";
 import type { THIREUS } from "../../types";
 import { db } from "../../database/db";
 import { httpResponse } from "../../utils/apiResponseUtils";
 import { gloabalEmailMessage } from "../../services/gloablEmailMessageService";
 import { ADMIN_MAIL_1 } from "../../config/config";
+import type { _Request } from "../../middlewares/authMiddleware";
 
 export default {
-  hireUs: asyncHandler(async (req, res) => {
+  // create hire us request controller
+  createHireUsRequest: asyncHandler(async (req, res) => {
     const files = req.files as Express.Multer.File[];
     // ** validation for this data is already handled in  middleware
     const data = req.body as THIREUS;
@@ -46,5 +48,45 @@ export default {
     });
     await gloabalEmailMessage(ADMIN_MAIL_1, data.email, ADMINNAME, HIREUSMESSAGE, "Request to hire Prime Logic Solution", `Dear ${data.name}`);
     httpResponse(req, res, SUCCESSCODE, SUCCESSMSG, { data: createdHireUs });
+  }),
+
+  // **  get All the hire us request
+  getAllHireUsRequests: asyncHandler(async (req, res) => {
+    const hireUs = await db.hireUs.findMany({ where: { trashedAt: null, trashedBy: null } });
+    if (hireUs.length === 0) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG, { data: hireUs });
+  }),
+
+  // ** get Single hire us request
+  getSingleHireUsRequest: asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const hireUs = await db.hireUs.findUnique({ where: { id: Number(id) } });
+    if (!hireUs) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG, { data: hireUs });
+  }),
+  // ** trash hire us request
+  trashHireUsRequest: asyncHandler(async (req: _Request, res) => {
+    const { id } = req.params;
+    const user = await db.user.findUnique({ where: { uid: req.userFromToken?.uid || "" } });
+    const hireUs = await db.hireUs.update({
+      where: { id: Number(id) },
+      data: { trashedAt: new Date(), trashedBy: `@${user?.username}-${user?.fullName}-${user?.role}` }
+    });
+    if (!hireUs) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG, { data: hireUs });
+  }),
+  // ** untrash hir us request
+  untrashHireUsRequest: asyncHandler(async (req: _Request, res) => {
+    const { id } = req.params;
+    const hireUs = await db.hireUs.update({ where: { id: Number(id) }, data: { trashedAt: null, trashedBy: null } });
+    if (!hireUs) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG, { data: hireUs });
+  }),
+  // ** permanent delete hire us request
+  permanentDeleteHireUsRequest: asyncHandler(async (req: _Request, res) => {
+    const { id } = req.params;
+    const hireUs = await db.hireUs.delete({ where: { id: Number(id) } });
+    if (!hireUs) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG);
   })
 };
--- src/routers/hireUsRouter/hireUsRouter.ts
+++ src/routers/hireUsRouter/hireUsRouter.ts
@@ -4,5 +4,31 @@ import { fileUploader } from "../../middlewares/multerMiddleware";
 import { validateDataMiddleware } from "../../middlewares/validationMiddleware";
 import { hireUsSchema } from "../../validation/zod";
 export const hireUsRouter = Router();
+import authMiddleware from "../../middlewares/authMiddleware";
+import rateLimiterMiddleware from "../../middlewares/rateLimiterMiddleware";
 
-hireUsRouter.route("/createHireUs").post(fileUploader, validateDataMiddleware(hireUsSchema), hireUsController.hireUs);
+hireUsRouter.route("/createHireUsRequest").post(
+  fileUploader,
+  validateDataMiddleware(hireUsSchema),
+
+  async (req, res, next) => {
+    await rateLimiterMiddleware.handle(req, res, next, 10, undefined, 10, 7200);
+  },
+  hireUsController.createHireUsRequest
+);
+
+hireUsRouter
+  .route("/getAllHireUsRequests")
+  .get(authMiddleware.checkToken, authMiddleware.checkIfUserIAdminOrModerator, hireUsController.getAllHireUsRequests);
+hireUsRouter
+  .route("/getSingleHireUsRequest/:id")
+  .get(authMiddleware.checkToken, authMiddleware.checkIfUserIAdminOrModerator, hireUsController.getSingleHireUsRequest);
+hireUsRouter
+  .route("/trashHireUsRequest/:id")
+  .patch(authMiddleware.checkToken, authMiddleware.checkIfUserIAdminOrModerator, hireUsController.trashHireUsRequest);
+hireUsRouter
+  .route("/untrashHireUsRequest/:id")
+  .patch(authMiddleware.checkToken, authMiddleware.checkIfUserIsAdmin, hireUsController.untrashHireUsRequest);
+hireUsRouter
+  .route("/permanentDeleteHireUsRequest/:id")
+  .delete(authMiddleware.checkToken, authMiddleware.checkIfUserIsAdmin, hireUsController.permanentDeleteHireUsRequest);
