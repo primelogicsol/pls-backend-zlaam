36dca680d649610f0f56cbb2ddda023636d3e904
zlaam <zlaam.dev@gmail.com>
2024-11-15 21:00:12 +0500

    feat(freeLanceController): Add isAcceptedField in freelancer modle

--- src/controllers/freeLancerController/freeLancerController.ts
+++ src/controllers/freeLancerController/freeLancerController.ts
@@ -1,12 +1,14 @@
 import { ADMIN_MAIL_1 } from "../../config/config";
-import { ADMINNAME, BADREQUESTCODE, SUCCESSCODE, SUCCESSMSG, THANKYOUMESSAGE } from "../../constants";
+import { ADMINNAME, BADREQUESTCODE, NOTFOUNDCODE, NOTFOUNDMSG, SUCCESSCODE, SUCCESSMSG, THANKYOUMESSAGE } from "../../constants";
 import { db } from "../../database/db";
+import type { _Request } from "../../middlewares/authMiddleware";
 import { gloabalEmailMessage } from "../../services/gloablEmailMessageService";
 import type { TFREELANCER } from "../../types";
 import { httpResponse } from "../../utils/apiResponseUtils";
 import { asyncHandler } from "../../utils/asyncHandlerUtils";
 
 export default {
+  // ** Freelancer will request for create an account
   getFreeLancerJoinUsRequest: asyncHandler(async (req, res) => {
     const freeLancer = req.body as TFREELANCER;
     const isExist = await db.freeLancers.findUnique({
@@ -27,6 +29,76 @@ export default {
       `Your Request To Join Our Team`,
       `Dear, ${freeLancer.name}`
     );
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG);
+  }),
+  //  ** Get all freelancer Request which are unaccepted
+  getAllFreeLancerRequest: asyncHandler(async (req, res) => {
+    const freelancers = await db.freeLancers.findMany({
+      where: {
+        trashedAt: null,
+        trashedBy: null,
+        isAccepted: false
+      }
+    });
+    if (freelancers.length === 0) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG, freelancers);
+  }),
+  // ** Get Single freelancer Request which are unaccepted
+  getSingleFreeLancerRequest: asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const freelancer = await db.freeLancers.findUnique({
+      where: {
+        id: Number(id),
+        trashedAt: null,
+        trashedBy: null
+      }
+    });
+    if (!freelancer) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG, freelancer);
+  }),
+
+  // ** Trash Freelancer Requst
+  trashFreeLancerRequest: asyncHandler(async (req: _Request, res) => {
+    const { id } = req.params;
+    const uid = req.userFromToken?.uid as string;
+
+    const userWhoTrashed = await db.user.findUnique({ where: { uid: uid } });
+    if (!userWhoTrashed) throw { status: BADREQUESTCODE, message: "User not found" };
+    await db.freeLancers.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        trashedBy: `@${userWhoTrashed.username}-${userWhoTrashed.fullName}-${userWhoTrashed.role}`,
+        trashedAt: new Date()
+      }
+    });
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG);
+  }),
+  // ** untrash freelancer Join request
+  untrashFreeLancerRequest: asyncHandler(async (req: _Request, res) => {
+    const { id } = req.params;
+    await db.freeLancers.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        trashedBy: null,
+        trashedAt: null
+      }
+    });
+    httpResponse(req, res, SUCCESSCODE, SUCCESSMSG);
+  }),
+  // ** Permanently delete the request
+  deleteFreeLancerRequest: asyncHandler(async (req: _Request, res) => {
+    const { id } = req.params;
+    const isRequestExist = await db.freeLancers.findUnique({ where: { id: Number(id) }, select: { id: true } });
+    if (!isRequestExist) throw { status: NOTFOUNDCODE, message: NOTFOUNDMSG };
+    await db.freeLancers.delete({
+      where: {
+        id: Number(id)
+      }
+    });
     httpResponse(req, res, SUCCESSCODE, SUCCESSMSG);
   })
 };
--- src/routers/freelancerRouter/freeLancerRouter.ts
+++ src/routers/freelancerRouter/freeLancerRouter.ts
@@ -2,6 +2,33 @@ import { Router } from "express";
 import { validateDataMiddleware } from "../../middlewares/validationMiddleware";
 import { freeLancerSchema } from "../../validation/zod";
 import freeLancerController from "../../controllers/freeLancerController/freeLancerController";
+import rateLimiterMiddleware from "../../middlewares/rateLimiterMiddleware";
+import authMiddleware from "../../middlewares/authMiddleware";
 export const freeLancerRouter = Router();
 
-freeLancerRouter.route("/getFreeLancerJoinUsRequest").post(validateDataMiddleware(freeLancerSchema), freeLancerController.getFreeLancerJoinUsRequest);
+freeLancerRouter
+  .route("/getFreeLancerJoinUsRequest")
+  .post(
+    validateDataMiddleware(freeLancerSchema),
+    (req, res, next) => rateLimiterMiddleware.handle(req, res, next, 10, undefined, 10, 300),
+    freeLancerController.getFreeLancerJoinUsRequest
+  );
+
+freeLancerRouter
+  .route("/getAllFreeLancerRequest")
+  .get(authMiddleware.checkToken, authMiddleware.checkIfUserIAdminOrModerator, freeLancerController.getAllFreeLancerRequest);
+
+freeLancerRouter
+  .route("/getSingleFreeLancerRequest/:id")
+  .get(authMiddleware.checkToken, authMiddleware.checkIfUserIAdminOrModerator, freeLancerController.getSingleFreeLancerRequest);
+freeLancerRouter
+  .route("/deleteFreeLancerRequest/:id")
+  .get(authMiddleware.checkToken, authMiddleware.checkIfUserIAdminOrModerator, freeLancerController.deleteFreeLancerRequest);
+
+freeLancerRouter
+  .route("/trashFreeLancerRequest/:id")
+  .patch(authMiddleware.checkToken, authMiddleware.checkIfUserIAdminOrModerator, freeLancerController.trashFreeLancerRequest);
+
+freeLancerRouter
+  .route("/untrashFreeLancerRequest/:id")
+  .patch(authMiddleware.checkToken, authMiddleware.checkIfUserIsAdmin, freeLancerController.untrashFreeLancerRequest);
